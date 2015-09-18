c416dd7c917da7ee3fde0ae00d51fd6195640ae7
aurelien <wolzaurelien@gmail.com>
2015-09-18 13:00:29 +0200

    Ajout de commentaires

--- Ulam.py
+++ Ulam.py
@@ -10,7 +10,7 @@ BLACK = (0x00, 0x00, 0x00, 0xff)
 def spiral(largeur, hauteur, img):
     # Initialisation des variables nécessaires
     x = y = 0
-    # Variable de déplacement
+    # Variables de déplacement
     pos_x = 0
     pos_y = -1
     # Nombre a tester
@@ -18,19 +18,23 @@ def spiral(largeur, hauteur, img):
     for i in range(largeur*hauteur):
         if (-largeur/2 < x <= largeur/2) and (-hauteur/2 < y <= hauteur/2):
 
-            # Test de primalité : on test si n n'est pas un nombre premier
             n += 1
             cx = (largeur/2)-x
             cy = (hauteur/2)-y
 
+            # Test de primalité : on test si n n'est pas un nombre premier
             if isprime(n) :
                 img.putpixel((cy,cx), tuple(BLACK))
 
+        # Détermine l'orientation du déplacement à effectuer
         if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
             pos_x, pos_y = -pos_y, pos_x
         x, y = x+pos_x, y+pos_y
 
+# Propriétés graphique de l'image résultante
 largeur = hauteur = 500
 graphique = Image.new('RGBA',(largeur,hauteur),color=(WHITE))
+
+# Appel de la fonction pour dessiner la spirale
 spiral(largeur, hauteur, graphique)
 graphique.show(command = 'eog')
